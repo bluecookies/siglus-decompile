86163ae7173f562b4dd73f0180fa2b678ecc7f42
bluecookies <landentire@gmail.com>
2018-07-18 15:46:43 +1000

    Merge from master

--- src/expression.rs
+++ src/expression.rs
@@ -3,8 +3,8 @@ use std;
 use ::Variable;
 use ::VariableType;
 use ::Function;
-use ::cfg::Instruction;
-use ::format_list;
+use Instruction;
+use format_list;
 
 ////////////////////////////////////////////////////////////////////////////////
 #[derive(Clone, Debug, PartialEq)]
@@ -391,15 +391,15 @@ impl std::fmt::Display for Expression {
 				ref function, option, ref args, ref extra_params, extra, ..
 			} => {
 				let option_str = if option == 0 { String::new() } else { format!("{{{}}}", option) };
-				let extra_param_str = if extra_params.is_empty() { String::new() } else { format!("<{}>", format_list(extra_params)) };
+				let extra_param_str = if extra_params.is_empty() { String::new() } else { format!("<{}>", format_list(extra_params.iter())) };
 				let extra_str = if let Some(extra) = extra { format!(", {}", extra) } else { String::new() };
 				if let &Expression::Variable(_) = function.as_ref() {
 					warn!("Function {} is a variable", function);
 				}
-				write!(f, "{}{}({}){}{}", function, option_str, format_list(args), extra_param_str, extra_str)
+				write!(f, "{}{}({}){}{}", function, option_str, format_list(args.iter()), extra_param_str, extra_str)
 			},
 			Expression::System(num) => write!(f, "sys_{:#x}", num),
-			Expression::List(ref vec) => write!(f, "{{{}}}", format_list(vec)),
+			Expression::List(ref vec) => write!(f, "{{{}}}", format_list(vec.iter())),
 			Expression::Error => write!(f, "ERROR"),
 
 			Expression::LocalVarRef(idx) => write!(f, "local_var[{}]", idx),
@@ -456,12 +456,12 @@ impl std::fmt::LowerHex for Expression {
 				ref function, option, ref args, ref extra_params, extra, ..
 			} => {
 				let option_str = if option == 0 { String::new() } else { format!("{{{}}}", option) };
-				let extra_param_str = if extra_params.is_empty() { String::new() } else { format!("<{}>", format_list(extra_params)) };
+				let extra_param_str = if extra_params.is_empty() { String::new() } else { format!("<{}>", format_list(extra_params.iter())) };
 				let extra_str = if let Some(extra) = extra { format!(", {}", extra) } else { String::new() };
-				write!(f, "{:#x}{}({}){}{}", function.as_ref(), option_str, format_list(args), extra_param_str, extra_str)
+				write!(f, "{:#x}{}({}){}{}", function.as_ref(), option_str, format_list(args.iter()), extra_param_str, extra_str)
 			},
 			Expression::System(num) => write!(f, "sys_{:#x}", num),
-			Expression::List(ref vec) => write!(f, "{{{}}}", format_list(vec)),
+			Expression::List(ref vec) => write!(f, "{{{}}}", format_list(vec.iter())),
 			Expression::Error => write!(f, "ERROR"),
 
 			Expression::LocalVarRef(idx) => write!(f, "local_var[{}]", idx),
